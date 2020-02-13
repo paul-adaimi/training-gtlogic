790a1d8fecfbc46f26fad2972acaaae2433690d9
paulgtlogic <paul@gtlogic.com>
2020-02-13 17:55:14 +0200

    homepage done

--- sharqiyya-app-homepage/webpack.config.js
+++ sharqiyya-app-homepage/webpack.config.js
@@ -13,5 +13,24 @@ module.exports = {
             template: "index.html",
             inject : "body"
         })
-    ]
+    ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      {
+        test:/\.css$/,
+        use:['style-loader','css-loader']
+      },
+    ],
+  },
 };
